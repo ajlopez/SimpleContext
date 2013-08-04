452cea347a1386207b726f5cce5b70eed4f80029
ajlopez <webmaster@ajlopez.com>
2013-08-04 11:44:57 -0300

    Additional test: context with two properties

--- test/assert.js
+++ test/assert.js
@@ -23,5 +23,12 @@ assert.equal(result.length, 2);
 assert.equal(result[0], 'foo');
 assert.equal(result[1], 'bar');
 
+// assert item on two key context
 
+space.assert('spam', { country: 'Japan', category: 'Beverages' });
+var result = space.retrieve({ country: 'Japan', category: 'Beverages' });
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.equal(result.length, 1);
+assert.equal(result[0], 'spam');
 
