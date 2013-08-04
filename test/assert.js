56dcd4fb445581b0520c0222ccf002ce42d7f16d
ajlopez <webmaster@ajlopez.com>
2013-08-04 11:43:23 -0300

    Additional test, second assert same context

--- test/assert.js
+++ test/assert.js
@@ -13,6 +13,15 @@ assert.ok(Array.isArray(result));
 assert.equal(result.length, 1);
 assert.equal(result[0], 'foo');
 
+// assert another item and retrieve context
+
+space.assert('bar', { country: 'UK' });
+var result = space.retrieve({ country: 'UK' });
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.equal(result.length, 2);
+assert.equal(result[0], 'foo');
+assert.equal(result[1], 'bar');
 
 
 
