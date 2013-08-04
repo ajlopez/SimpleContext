23779e246a0eb88e892379ea621c7aed40bae925
ajlopez <webmaster@ajlopez.com>
2013-08-04 12:17:34 -0300

    Two levels inheritance, wo/repetition

--- test/inheritance.js
+++ test/inheritance.js
@@ -18,3 +18,16 @@ assert.ok(result.indexOf('foo') >= 0);
 assert.ok(result.indexOf('bar') >= 0);
 assert.ok(result.indexOf('spam') >= 0);
 
+// additional data in new context, retrieve with inheritance
+
+space.assert('joedoe', { country: 'UK', category: 'Beverages', company: 'Acme' });
+
+var result = space.retrieve({ country: 'UK', company: 'Acme', category: 'Beverages' });
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.equal(result.length, 4);
+assert.ok(result.indexOf('foo') >= 0);
+assert.ok(result.indexOf('bar') >= 0);
+assert.ok(result.indexOf('spam') >= 0);
+assert.ok(result.indexOf('joedoe') >= 0);
+
